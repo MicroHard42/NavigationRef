2be272f6c70051dd55b64872736e21c237193e0b
MicroHard42 <v47a47k47@live.com>
2024-09-28 10:23:46 -0500

    Initial Commit

---
+++ NavigationRef/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NavigationRef
+//
+//  Created by Adam Kuhnel on 9/28/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NavigationRefTests/NavigationRefTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationRefTests.swift
+//  NavigationRefTests
+//
+//  Created by Adam Kuhnel on 9/28/24.
+//
+
+import Testing
+@testable import NavigationRef
+
+struct NavigationRefTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
